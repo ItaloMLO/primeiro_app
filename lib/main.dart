4ecb279c34d374499b81d340f77fd1097c2d54a9
ItaloMLO <italomlo12@gmail.com>
2023-05-30 01:13:33 -0300

    translate text

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Você clicou no botão esse tanto:',
             ),
             Text(
               '$_counter',
